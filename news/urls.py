615de8b22ae73e9d35992c7c97d367441e13bc62
Xusrav <xusrav200329@gmail.com>
2023-04-18 21:41:36 +0500

    Create new detail view for watch article, create new detail.html and add JavaScript & Ajax code, i never work with Ajax, but i write code with watching a bloger. Than i run server for chek workly

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('home/', home, name='home'),
+    path('article/<int:pk>/detail/', article_detail, name='article_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -13,3 +13,8 @@ def home(request):
     articles = Article.objects.filter(is_activate=True)
     return render(request, 'news/home.html', {'articles': articles})
 
+
+def article_detail(request, pk):
+    article = Article.objects.get(id=pk)
+    articles = Article.objects.filter(is_activate=True)
+    return render(request, 'news/detail.html', context={'article': article, 'articles': articles})
