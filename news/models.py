f07dae344e85b7221cade8d8a94448012122dda6
Xusrav <xusrav200329@gmail.com>
2023-04-18 20:44:20 +0500

    Create Article model in models.py and create superuser, than i make migrations, than run server and add some Articles from admin panel

--- news/models.py
+++ news/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_activate = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
