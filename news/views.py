6de11dcdcab7799ed01f44fbb7cce94d88f174aa
Xusrav <xusrav200329@gmail.com>
2023-04-18 21:00:49 +0500

    Create home view and add to urlpatterns list, than create home.html and add some code for optimize our web-site.

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Article
 
 
 # Create your views here.
@@ -6,3 +7,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'news/index.html')
+
+
+def home(request):
+    articles = Article.objects.filter(is_activate=True)
+    return render(request, 'news/home.html', {'articles': articles})
+
