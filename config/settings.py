59389cac3dba0ef38a108c6d877d66a77512f511
Xusrav <xusrav200329@gmail.com>
2023-04-21 17:58:50 +0500

    Run server and chek how it's work

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "login"
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
